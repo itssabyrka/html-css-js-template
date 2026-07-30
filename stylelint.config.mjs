23fd0a2a8d33a1386cfed4c3dff98beebc4a9595
Sabyrka <itssabyrka@gmail.com>
2026-07-30 07:33:49 +0500

    chore: update stylelint configuration

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'import-notation': null,
+    'font-family-name-quotes': null,
+  },
 }
